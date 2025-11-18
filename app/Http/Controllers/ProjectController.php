07536ee3398fc8940c2aea896b6ee3cce0d9339b
DevWithShakil <m.shakil720@gmail.com>
2025-11-18 12:19:17 +0600

    project create controller updated

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
@@ -11,7 +12,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        //
+        return Project::all();
     }
 
     /**
@@ -19,7 +20,23 @@ class ProjectController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'title' => 'required',
+            'slug' => 'required|unique:projects',
+            'description' => 'required',
+            'tech_stack' => 'required',
+            'thumbnail' => 'nullable',
+            'github_link' => 'nullable',
+            'live_link' => 'nullable',
+        ]);
+
+        // Insert the project into the database
+        $project = Project::create($validated);
+
+        return response()->json([
+            'message' => 'Project created successfully',
+            'data' => $project,
+        ], 201);
     }
 
     /**
