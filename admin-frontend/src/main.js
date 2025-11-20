f69daaa273b4c86ff8149239e5d251100417bc9f
DevWithShakil <m.shakil720@gmail.com>
2025-11-20 22:49:04 +0600

    toast updated for projects

--- admin-frontend/src/main.js
+++ admin-frontend/src/main.js
@@ -2,11 +2,20 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
+const app = createApp(App);
 
-createApp(App).use(router, Toast, {
+// First use router
+app.use(router);
+
+// Then use toast
+app.use(Toast, {
     position: "top-right",
     timeout: 2500,
-}).mount('#app');
+});
+
+// Finally mount
+app.mount('#app');
